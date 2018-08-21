cef9c029c4c92ea698a9a04c0793c5c895d4b322
ecssiah <ecssiah@gmail.com>
2018-08-21 11:23:58 -0500

    Add CRUD actions to focus controller

--- app/controllers/api/v1/focuses_controller.rb
+++ app/controllers/api/v1/focuses_controller.rb
@@ -7,6 +7,43 @@ class FocusesController < ApplicationController
     render json: @focuses
   end
 
+  def show
+    @focus = Focus.find(params[:id])
+    render json: @focus
+  end
+
+  def create
+    @focus = Focus.new(focus_params)
+
+    if @focus.save
+      render json: @focus, status: :created
+    else
+      render json: @focus.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @focus = Focus.find(params[:id])
+    
+    if @focus.update(focus_params)
+      render json: @focus
+    else
+      render json: @focus.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @focus = Focus.find(params[:id])
+    @focus.destroy
+
+    if @focus.destroy
+      head :no_content, status: :ok
+    else
+      render json: @focus.errors, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def focus_params
