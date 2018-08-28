026f432190b1aed5d323930dc756ca9c8b07ff6e
ecssiah <ecssiah@gmail.com>
2018-08-27 19:46:11 -0500

    Move initial state outside of combineReducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,34 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import rootReducer from './reducers/root-reducer' 
+import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(
-  rootReducer, 
-  { 
-    header: {
-      page: 'selection',
-    },
-    selection: [],
-    focus: {
-      title: '',
-      time: 0,
-      goal: 10,
-      level: 0,
-    },
-    settings: {
-      workPeriod: 20,
-      breakPeriod: 2,
-      backgroundColor: '#8e7985',
-      progressColor: '#dc48ad',
-    }, 
-  }
-)
+const initialState = { 
+  header: {
+    page: 'selection',
+  },
+  selection: [],
+  focus: {
+    title: '',
+    time: 0,
+    goal: 10,
+    level: 0,
+  },
+  settings: {
+    workPeriod: 20,
+    breakPeriod: 2,
+    backgroundColor: '#8e7985',
+    progressColor: '#dc48ad',
+  }, 
+}
 
+const store = createStore(rootReducer, initialState)
 
 ReactDOM.render(
   <Provider store={store}>
