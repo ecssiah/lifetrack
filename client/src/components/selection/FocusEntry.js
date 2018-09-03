d30bc1ac2fc575a71f926600bb9b3babca5142bc
ecssiah <ecssiah@gmail.com>
2018-09-03 14:30:45 -0500

    Conditionally render delete button when hovering

--- client/src/components/selection/FocusEntry.js
+++ client/src/components/selection/FocusEntry.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 import { Grid, Row, Col, ProgressBar } from 'react-bootstrap'
 import './styles.css'
 
-const FocusEntry = props => {
-  return (
-    <Grid>
-      <Row>
-        <NavLink to={`/focuses/${props.focus.id}`}>
-          <Col xs={6} md={6}>
-            <span className='entry'>{props.focus.name}</span>
+class FocusEntry extends Component { 
+  state = {
+    isHovering: false
+  }
+
+  handleMouseHover = event => {
+    
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row 
+          onMouseEnter={() => this.setState({isHovering: true})}
+          onMouseLeave={() => this.setState({isHovering: false})}
+        > 
+          <NavLink to={`/focuses/${this.props.focus.id}`}>
+            <Col xs={6} md={6}>
+              <span className='entry'>{this.props.focus.name}</span>
+            </Col>
+            <Col xs={2} md={2}>
+              <span className='entry'>{`Lvl: ${this.props.focus.level}`}</span>
+            </Col>
+            <Col xs={3} md={3}> 
+              <ProgressBar now={this.props.focus.experience} />
+            </Col>
+          </NavLink>
+          <Col xs={1} md={1}>
+            <div className='delete'>
+              {
+                this.state.isHovering &&
+                <input 
+                  type='image' src='delete-button.svg' alt='delete-button' 
+                />
+              }
+            </div>
           </Col>
-          <Col xs={2} md={2}>
-            <span className='entry'>{`Lvl: ${props.focus.level}`}</span>
-          </Col>
-          <Col xs={3} md={3}> 
-            <ProgressBar now={props.focus.experience} />
-          </Col>
-        </NavLink>
-        <Col xs={1} md={1}>
-          <input className='delete' type='image' src='delete-button.svg' />
-        </Col>
-      </Row>
-    </Grid>
-  )
+        </Row>
+      </Grid>
+    )
+  }
 }
 
 export default FocusEntry
