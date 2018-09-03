4592f2f0646c002a119dc9fcc77915067c0b36e1
ecssiah <ecssiah@gmail.com>
2018-09-03 13:12:16 -0500

    Fix error in rails routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :focuses, only: [:index, :create, :update, :destroy]
-      resource :settings, only: [:index, :update]  
+      resources :focuses, only: [:index, :create, :update, :destroy]
+      resources :settings, only: [:index, :update]  
     end
   end
 end
