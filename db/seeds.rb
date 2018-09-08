c114f0d19116710945e5f7d5340cb39cf6bca34a
ecssiah <ecssiah@gmail.com>
2018-09-08 17:29:51 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,16 @@
-Setting.create(
-  name: "Work Period",
-  value: "20"
-)
+Setting.create(name: "Work Period", value: "20")
+Setting.create(name: "Break Period", value: "2")
+Setting.create(name: "Goal", value: "10")
 
-Setting.create(
-  name: "Break Period",
-  value: "2"
-)
-
-Setting.create(
-  name: "Goal",
-  value: "10"
+Focus.create(
+  name: "Running", level: 3, experience: 98.2, iterations: 0, goal: 10
 )
 
 Focus.create(
-  name: "LifeTrack Project",
-  level: 3, experience: 98.2,
-  iterations: 0, goal: 10
+  name: "Web Development", level: 1, experience: 24.8, iterations: 0, goal: 12
 )
 
 Focus.create(
-  name: "Running",
-  level: 1, experience: 24.8,
-  iterations: 0, goal: 12
-)
-
-Focus.create(
-  name: "Green Fractals Project",
-  level: 12, experience: 0.0,
-  iterations: 0, goal: 6
+  name: "Cardio", level: 12, experience: 0.0, iterations: 0, goal: 6
 )
 
