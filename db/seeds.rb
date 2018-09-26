9fe846b34fc6e6f2f9f628faf5a9f84a56d280a5
ecssiah <ecssiah@gmail.com>
2018-09-26 13:04:35 -0500

    Update seed file for heroku build

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
 Setting.create(name: "Work Period", value: "20")
 Setting.create(name: "Break Period", value: "2")
 Setting.create(name: "Goal", value: "10")
-
-Focus.create(
-  name: "Running", 
-  level: 3, experience: 99.7, 
-  time: 0, periods: 0
-)
-
-Focus.create(
-  name: "Web Development", 
-  level: 1, experience: 24.8,
-  time: 0, periods: 0
-)
-
-Focus.create(
-  name: "Study Greek", 
-  level: 12, experience: 0.0,
-  time: 0, periods: 0
-)
-
